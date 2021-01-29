b2aaf7ac7679a9b2efa29f26d4f6c404aefc72e7
Jelvani <aj654@rutgers.edu>
2021-01-29 14:34:49 -0500

    orbiting cubes

--- draw.js
+++ draw.js
@@ -28,15 +28,15 @@ canvas.addEventListener('mousemove', e => {
     }
   });
 
-function drawPoint(x,y,x1,y1,color){
+function drawPoint(x,y,x1,y1,color1,color2){
     x = x+canvas.width/2;
     y = y+canvas.height/2;
     x1 = x1+canvas.width/2;
     y1 = y1+canvas.height/2;
 
     var grad= ctx.createLinearGradient(x, y, x1, y1);
-    grad.addColorStop(0, color);
-    grad.addColorStop(1, "green");
+    grad.addColorStop(0, color1);
+    grad.addColorStop(1, color2);
 
     ctx.strokeStyle = grad;
     
@@ -60,10 +60,26 @@ cube = [[1,1,-1],
         [-1,-1,1],
         [-1,1,1]];
 
+pentagon = [[-1,2,-1],
+            [1,2,-1],
+            [2,1,-1],
+            [2,-1,-1],
+            [1,-2,-1],
+            [-1,-2,-1],
+            [-2,-1,-1],
+            [-2,1,-1],
+            [-1,2,1],
+            [1,2,1],
+            [2,1,1],
+            [2,-1,1],
+            [1,-2,1],
+            [-1,-2,1],
+            [-2,-1,1],
+            [-2,1,1]];
+//apply 3D rotation to a single point around (0,0,0)
+function rotate(pt, Rx,Ry,Rz){
 
-
-function drawCube(x,y,z,Rx,Ry,Rz,scale,color){
-    
+    //rotation matrices
     matX = [[1,0,0],
     [0,Math.cos(Rx),-1*Math.sin(Rx)],
     [0,Math.sin(Rx),Math.cos(Rx)]];
@@ -76,51 +92,98 @@ function drawCube(x,y,z,Rx,Ry,Rz,scale,color){
     [Math.sin(Rz),Math.cos(Rz),0],
     [0,0,1]];
 
+      
+    pt = math.multiply(matX,pt)
+    pt = math.multiply(matY,pt)
+    pt = math.multiply(matZ,pt)
+    return pt;
+}
+
+function translate(pt, x,y,z){
+    pt[0] = pt[0] + x;
+    pt[1] = pt[1] + y;
+    pt[2] = pt[2] + z;
+}
+
+//project with perspective projection
+function project(pt){
+    //perspective projection matrix
+    pMat = [[5*math.tan(100/pt[2]),0,0],
+            [0,5*math.tan(100/pt[2]),0]];
+
+    tmp=math.multiply(pMat,pt);
+    return tmp;
+
+}
+function drawCube(x,y,z,Rx,Ry,Rz,scale,color1,color2){
     newP = []
+    //for each points in cube
     for(pt of cube){
-        tmp = pt
-        
-        tmp = math.multiply(matX,tmp)
-        tmp = math.multiply(matY,tmp)
-        tmp = math.multiply(matZ,tmp)
 
-        
-        
-        
-        //translate cube to wherever
-        tmp[0] = tmp[0] + x;
-        tmp[1] = tmp[1] + y;
-        tmp[2] = tmp[2] + z;
-
+        //rotate around 3 axis
+        tmp=rotate(pt,Rx,Ry,Rz)
+        //translate cube on 3 axis
+        translate(tmp,x,y,z)
         //scale cube
         tmp=math.multiply(tmp,scale);
-
-        tmp[0] = Math.round(tmp[0] *math.tan((100/tmp[2]))*5);
-        tmp[1] = Math.round(tmp[1] *math.tan((100/tmp[2]))*5);
-
-        
+        tmp = project(tmp);
         newP.push(tmp)
     }
     for(i =0;i<4;i++){
         if(i>0){
-            drawPoint(newP[i-1][0],newP[i-1][1],newP[i][0],newP[i][1],color);
+            drawPoint(newP[i-1][0],newP[i-1][1],newP[i][0],newP[i][1],color1,color2);
         }
     }
-    drawPoint(newP[0][0],newP[0][1],newP[3][0],newP[3][1],color);
+    drawPoint(newP[0][0],newP[0][1],newP[3][0],newP[3][1],color1,color2);
 
     for(i =4;i<8;i++){
         if(i>4){
-            drawPoint(newP[i-1][0],newP[i-1][1],newP[i][0],newP[i][1],color);
+            drawPoint(newP[i-1][0],newP[i-1][1],newP[i][0],newP[i][1],color1,color2);
         }
     }
-    drawPoint(newP[4][0],newP[4][1],newP[7][0],newP[7][1],color);
+    drawPoint(newP[4][0],newP[4][1],newP[7][0],newP[7][1],color1,color2);
 
     for(i=0;i<4;i++){
-        drawPoint(newP[i][0],newP[i][1],newP[i+4][0],newP[i+4][1],color);
+        drawPoint(newP[i][0],newP[i][1],newP[i+4][0],newP[i+4][1],color1,color2);
     }
 }
 
+function drawPentagon(x,y,z,Rx,Ry,Rz,scale,color1,color2){
+    newP = []
+    //for each points in cube
+    for(pt of pentagon){
 
+        //rotate around 3 axis
+        tmp=rotate(pt,Rx,Ry,Rz)
+        //translate cube on 3 axis
+        translate(tmp,x,y,z)
+        tmp=math.multiply(tmp,scale);
+        tmp = project(tmp);
+        //scale cube
+        
+        newP.push(tmp)
+    }
+    for(i =0;i<8;i++){
+        if(i>0){
+            drawPoint(newP[i-1][0],newP[i-1][1],newP[i][0],newP[i][1],color1,color2);
+        }
+    }
+    drawPoint(newP[0][0],newP[0][1],newP[7][0],newP[7][1],color1,color2);
+
+    
+    for(i =8;i<16;i++){
+        if(i>8){
+            drawPoint(newP[i-1][0],newP[i-1][1],newP[i][0],newP[i][1],color1,color2);
+        }
+    }
+    drawPoint(newP[8][0],newP[8][1],newP[15][0],newP[15][1],color1,color2);
+    
+    for(i=0;i<8;i++){
+        drawPoint(newP[i][0],newP[i][1],newP[i+8][0],newP[i+8][1],color1,color2);
+    }
+    
+    
+}
 setInterval(frame, 60);
 Rx = 0
 Ry = 0
@@ -134,10 +197,11 @@ function frame(){
     size1 = math.sin(Rx)*100+10;
     size2 = math.cos(Rx)*100+10;
     size3 = math.cos(Rx)*50+10;
-    //drawCube(rl,0,distance,math.sin(Ry),math.sin(Rz),math.sin(Rz),100,"Pink");
-    //drawCube(rl,0,distance,math.sin(Rx),math.sin(Ry),math.sin(Rz),100,"Lime");
-    drawCube(rl,0,distance,math.sin(Ry),math.sin(Rz),math.sin(Rx),100,"Blue");
-    //drawCube(rl,0,distance,0,0,0,200,"Red");
+    drawCube(5*math.sin(Ry)*math.sin(Ry/2)+rl,5*math.sin(Ry)*math.cos(Ry/2),distance,math.sin(Ry),math.sin(Rz),math.sin(Rz),100,"Pink","White");
+    drawCube(5*math.cos(Ry)+rl,5*math.sin(Ry),distance,math.sin(Rx),math.sin(Ry),math.sin(Rz),100,"Lime",'Purple');
+    //drawCube(rl,0,distance,math.sin(Ry),math.sin(Rz),math.sin(Rx),100,"Blue");
+    drawCube(rl,0,distance,math.sin(Ry),math.sin(Rz),math.sin(Rx),500,"Pink","Lime");
+    drawPentagon(rl,0,distance,math.sin(Rz),math.sin(Rx),math.sin(Rx),500,"Yellow","Blue");
 
 }
     
