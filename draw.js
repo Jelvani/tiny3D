860e2e75a8337444631639376f8da001d1067977
Jelvani <aj654@rutgers.edu>
2021-01-29 02:21:34 -0500

    added perspective

--- draw.js
+++ draw.js
@@ -6,6 +6,8 @@ ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 mouseX = 0
 mouseY = 0
+distance = -3;
+rl = 1;
 canvas.addEventListener('mousemove', e => {
 
     mouseX = e.offsetX;
@@ -13,10 +15,31 @@ canvas.addEventListener('mousemove', e => {
     
   });
 
+  document.addEventListener("keydown", e =>{
+    var keyCode = e.keyCode
+    if (keyCode === 38) {//up
+        distance+=0.1;
+    }else if(keyCode === 40){//down
+        distance-=0.1;
+    }else if(keyCode === 37){//left
+        rl-=0.1;
+    }else if(keyCode === 39){//right
+        rl+=0.1;
+    }
+  });
+
 function drawPoint(x,y,x1,y1,color){
+    x = x+canvas.width/2;
+    y = y+canvas.height/2;
+    x1 = x1+canvas.width/2;
+    y1 = y1+canvas.height/2;
+
+    var grad= ctx.createLinearGradient(x, y, x1, y1);
+    grad.addColorStop(0, color);
+    grad.addColorStop(1, "green");
+
+    ctx.strokeStyle = grad;
     
-    
-    ctx.strokeStyle = color;
     ctx.beginPath();
     //ctx.arc(x, y, 7, 0, 2 * Math.PI);
     ctx.moveTo(x, y);
@@ -55,18 +78,27 @@ function drawCube(x,y,z,Rx,Ry,Rz,scale,color){
 
     newP = []
     for(pt of cube){
-        tmp = math.multiply(matX,pt)
+        tmp = pt
+        
+        tmp = math.multiply(matX,tmp)
         tmp = math.multiply(matY,tmp)
         tmp = math.multiply(matZ,tmp)
+
+        
+        
+        
+        //translate cube to wherever
+        tmp[0] = tmp[0] + x;
+        tmp[1] = tmp[1] + y;
+        tmp[2] = tmp[2] + z;
 
         //scale cube
         tmp=math.multiply(tmp,scale);
 
-        //move cube to wherever
+        tmp[0] = Math.round(tmp[0] *math.tan((100/tmp[2]))*5);
+        tmp[1] = Math.round(tmp[1] *math.tan((100/tmp[2]))*5);
+
         
-        tmp[0] = tmp[0] + x;
-        tmp[1] = tmp[1] + y;
-        tmp[2] = tmp[2] + z;
         newP.push(tmp)
     }
     for(i =0;i<4;i++){
@@ -102,10 +134,10 @@ function frame(){
     size1 = math.sin(Rx)*100+10;
     size2 = math.cos(Rx)*100+10;
     size3 = math.cos(Rx)*50+10;
-    drawCube(mouseX,mouseY,0,Rz,Ry,Rx,size1,"Pink");
-    drawCube(mouseX,mouseY,0,Rx,Ry,Rz,size2,"Lime");
-    drawCube(mouseX,mouseY,0,Ry,Rz,Rx,size3,"Blue");
-    drawCube(mouseX,mouseY,0,Ry*2,Rz,Rx,size3*1.5,"Red");
+    //drawCube(rl,0,distance,math.sin(Ry),math.sin(Rz),math.sin(Rz),100,"Pink");
+    //drawCube(rl,0,distance,math.sin(Rx),math.sin(Ry),math.sin(Rz),100,"Lime");
+    drawCube(rl,0,distance,math.sin(Ry),math.sin(Rz),math.sin(Rx),100,"Blue");
+    //drawCube(rl,0,distance,0,0,0,200,"Red");
 
 }
     
